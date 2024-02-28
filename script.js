61590f1e0e1691f12735ba7a759a36508084cb7a
ifagan5 <ifagan27@students.stab.org>
2024-02-28 10:47:17 -0500

    function outlines

--- script.js
+++ script.js
@@ -3,3 +3,23 @@
     - Which major has the highest unemployment rate in a major category (string)
     - Most (and least?) popular major in a major category (string)
     - If a major is female or male dominated (string) */
+
+function getMajors() {
+
+}
+
+function highestUnemployment() {
+
+}
+
+function mostPopular() {
+
+}
+
+function femaleDominated() {
+
+}
+
+function dominantGender() {
+    
+}
