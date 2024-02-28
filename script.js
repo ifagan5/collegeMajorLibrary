75d0c95ddff5b382ef2bb58437957bc503cc068d
ifagan5 <ifagan27@students.stab.org>
2024-02-28 10:56:44 -0500

    Documentation

--- script.js
+++ script.js
@@ -4,22 +4,58 @@
     - Most (and least?) popular major in a major category (string)
     - If a major is female or male dominated (string) */
 
+/*DOCUMENTATION:
+Take a desired major category as a parameter and returns a list of majors in 
+that category.
+category -- (String) desried major category
+return - (List) all majors within that major category
+*/
 function getMajors() {
 
 }
 
+
+/*DOCUMENTATION:
+Takes a major category as a parameter and returns the name of the
+major with the highest unemployment rate
+category -- (String) desired major category
+return -- (String) the name of the major with the highest umemployment rate
+*/
 function highestUnemployment() {
 
 }
 
+
+/*DOCUMENTATION:
+Takes a major category as a parameter and returns the name of the most
+popular major in that major category
+category -- (String) desired major category
+return -- (String) the name of the most popular major in that
+category
+*/
 function mostPopular() {
 
 }
 
+
+/*DOCUMENTATION:
+Takes the desired major category as a parameter and returns a list of all
+female dominated majors within that major category.
+category -- (String) desired major category
+return -- (List) Like of all the names of the female dominated majors 
+in that library
+*/
 function femaleDominated() {
 
 }
 
+
+/*DOCUMENTATION:
+Takes the name of a major as a parameter and returns whether or not
+the major is female or male dominated
+majorName -- (String) the name of the desired major
+return -- (String) whether the major is female or male dominated
+*/
 function dominantGender() {
-    
+
 }
