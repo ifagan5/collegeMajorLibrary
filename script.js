e6fc30cd09d4bd86cd064ff7dd6d256639f41c4b
ifagan5 <ifagan27@students.stab.org>
2024-02-28 10:45:24 -0500

    beginning

---
+++ script.js
@@ -0,0 +1,5 @@
+/*    - Women dominated majors (list)
+    - List of majors in a major category (list)
+    - Which major has the highest unemployment rate in a major category (string)
+    - Most (and least?) popular major in a major category (string)
+    - If a major is female or male dominated (string) */
